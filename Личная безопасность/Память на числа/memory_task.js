23105895f1d35b9d385535b9adf71f9d6dfd360f
vesloguzov <vesloguzov@gmail.com>
2019-08-19 20:23:55 +0500

    fd

--- Личная безопасность/Память на числа/memory_task.js
+++ Личная безопасность/Память на числа/memory_task.js
@@ -2,35 +2,172 @@ function MemoryTask(settings) {
 
     let data = settings.data;
     let element = settings.element;
-    let student_state = {};
+    let state = {};
+    state.step = 0;
+    state.answer = {};
+    state.result = [];
+
+    let correct_map = [];
+    // element.classList.add("");
 
     let language = "ru";
     let labels = {
         ru: {
-            start_label: "Начать"
+            start_label: "Начать",
+            input_instruction: "Введите что-нибудь:",
+            start_instruction: ["Пожалуйста, не записывайте цифры, а только запоминайте.", "Тренажер предназначен для оценки кратковременной зрительной памяти, ее объема и точности."]
         },
-        en:{
-            start_label: "Начать"
+        en: {
+            start_label: "Начать",
+            input_instruction: "Введите что-нибудь:",
+            start_instruction: ["Пожалуйста, не записывайте цифры, а только запоминайте.", "Тренажер предназначен для оценки кратковременной зрительной памяти, ее объема и точности."]
         }
     };
 
+    let delay = 20;
+
     let user_labels = labels[language];
+
+    let input_selector = "#memory_input";
+    let answer = undefined;
+    let submit_btn = undefined;
+    let save_btn = undefined;
+    let problem_action_buttons_wrapper;
+    let submit_attempt_container;
+
+    if (document.querySelector(input_selector)) {
+        answer = new Answer(document.querySelector(input_selector).querySelector("input[type='text']"));
+        submit_btn = answer.elementField.closest(".problem").querySelector(".submit.btn-brand");
+
+        submit_btn.classList.add("hidden");
+        submit_btn.classList.add("submit-button-center");
+        submit_btn.removeAttribute("disabled");
+
+        save_btn = answer.elementField.closest(".problem").querySelector("button.save");
+        answer.elementField.closest(".problem").querySelector(".problem-action-buttons-wrapper").classList.add("hidden");
+        answer.elementField.closest(".problem").querySelector(".notification").classList.add("hidden");
+        answer.elementField.closest(".problem").querySelector(".submission-feedback").classList.add("hidden");
+        answer.elementField.closest(".problem").querySelector(".submit-attempt-container").classList.add("center-button-wrap");
+
+        if (answer.get()) {
+            state = answer.getJSON()["state"];
+            if(state.step === 2){
+                let msg = answer.elementField.closest(input_selector).querySelector(".message").innerHTML.toLocaleLowerCase();
+                if(msg.length > 0){
+                    state.result = JSON.parse(msg);
+                }
+            }
+        }
+    }
+
 
     let MemoryTaskInit = {
         init: function () {
-            console.log(data, element);
-            this.buildStart();
+                if (state.step === 0){
+                        this.buildStart();
+                }
+                else if (state.step === 1){
+                    this.buildInput();
+                }
+                else{
+                    this.buildResult();
+                }
         },
 
         buildStart: function () {
             let start_wrap = utils.create("div", {className: "start-wrapper"});
+
+            let instruction_block = utils.create("div", {className: "instruction-block"}, );
+
+            user_labels.start_instruction.map((inst)=>utils.create("p", {text: inst})).forEach(item=>instruction_block.appendChild(item));
+
+
             let start_btn = utils.create("button", {className: "", text: user_labels.start_label});
             start_btn.onclick = function (evt) {
-                console.log("Начали");
-            };
+                this.buildTable();
+            }.bind(this);
+
+            start_wrap.appendChild(instruction_block);
             start_wrap.appendChild(start_btn);
             element.appendChild(start_wrap);
-        }
+        },
+
+        buildTable: function () {
+            utils.clear(element);
+            let table_wrap = utils.create("div", {className: "table-wrapper"});
+            let timer_block = utils.create("div", {className: "timer-block"});
+            let table = utils.create("div", {className: "num-table-wrap"});
+            data.forEach(function (item) {
+                let num_item = utils.create("div", {className: "num-table-item", text: item});
+                table.appendChild(num_item);
+            });
+            table_wrap.appendChild(timer_block);
+            table_wrap.appendChild(table);
+            element.appendChild(table_wrap);
+            utils.timer(delay, function (s) {timer_block.innerHTML = utils.sec2time(s)}, this.buildInput);
+        },
+
+        buildInput: function () {
+            utils.clear(element);
+            state.step = 1;
+            answer.setJSON({"state": state});
+            save_btn.click();
+            let input_wrap = utils.create("div", {className: "input-wrapper"});
+            let input_instruction = utils.create("div", {className: "input-instruction", text: user_labels.input_instruction});
+            let input_area = utils.create("textarea", {className: "input-area", attr: {"rows": "2", "type": "text"}});
+
+            input_wrap.appendChild(input_instruction);
+            input_wrap.appendChild(input_area);
+            element.appendChild(input_wrap);
+
+            submit_btn.classList.remove("hidden");
+            answer.setJSON({"state": state});
+            let len = 0;
+
+            submit_btn.classList.add( "disabled");
+
+
+            input_area.oninput = function(evt){
+                this.value = this.value.replace(/[^\d, \t\n]/g,'');
+
+                let answer_arr_candidate = this.value.split(/[ ,\t\n]+/).filter(function (item) { return !isNaN(parseFloat(item))}).map(function(item){return parseInt(item)});
+
+                if(answer_arr_candidate.length < 1){
+                    submit_btn.classList.add("disabled");
+                }
+                else{
+                    submit_btn.classList.remove("disabled");
+                }
+
+                if(answer_arr_candidate.length > 12){
+                    this.value = this.value.substr(0, len);
+                    alert("Максимум элементов!");
+                }
+                else{
+                    len = this.value.length;
+                    state.answer = answer_arr_candidate;
+                    answer.setJSON({"state": state});
+                }
+
+                submit_btn.removeAttribute("disabled");
+                state.step = 2;
+            };
+
+        },
+        buildResult: function () {
+            utils.clear(element);
+            let result_wrap = utils.create("div", {className: "table-wrapper"});
+            let table = utils.create("div", {className: "num-table-wrap"});
+
+            data.forEach(function (item, idx) {
+                let num_item = utils.create("div", {className: "num-table-item" + (state.result[idx] ? " correct": ""), text: item});
+                table.appendChild(num_item);
+            });
+            result_wrap.appendChild(table)
+            element.appendChild(result_wrap);
+
+        },
+
 
     };
 
@@ -74,6 +211,65 @@ function MemoryTask(settings) {
 
             return element
         },
+        clear: function (el) {
+            el.innerHTML = "";
+        },
+
+        timer: function (seconds, tick, result) {
+            if (seconds !== -1) {
+                tick(seconds);
+                seconds -= 1;
+                setTimeout(function () {
+                    this.timer(seconds, tick, result);
+                }.bind(this), 1000);
+            } else {
+                result();
+            }
+        },
+        sec2time: function (timeInSeconds) {
+            let pad = function (num, size) {
+                    return ('000' + num).slice(size * -1);
+                },
+                time = parseFloat(timeInSeconds).toFixed(3),
+                hours = Math.floor(time / 60 / 60),
+                minutes = Math.floor(time / 60) % 60,
+                seconds = Math.floor(time - minutes * 60),
+                milliseconds = time.slice(-3);
+
+            return pad(minutes, 2) + ':' + pad(seconds, 2)
+            // return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2) + ',' + pad(milliseconds, 3);
+        }
+
+    };
+
+    function Answer(elementField) {
+        this.elementField = elementField;
+        this.fieldValue = "";
+        this.fieldObject = {};
+        this.get = function () {
+            this.fieldValue = elementField.value;
+            return this.fieldValue;
+        };
+        this.set = function (value) {
+            if (value === undefined) value = this.fieldValue;
+            elementField.value = value;
+        };
+        this.getJSON = function () {
+            if (this.isJsonString(this.get())) this.fieldObject = JSON.parse(this.get());
+            return this.fieldObject;
+        };
+        this.setJSON = function (object) {
+            if (object === undefined) object = this.fieldObject;
+            this.set(JSON.stringify(object))
+        };
+        this.isJsonString = function (str) {
+            try {
+                JSON.parse(str);
+            } catch (e) {
+                return false;
+            }
+            return true;
+        };
     };
 
     MemoryTaskInit.init();
