27ffef05d3669d383b9a1cbc1ddecc6139c14790
vesloguzov <vesloguzov@gmail.com>
2019-09-09 20:17:51 +0500

    dfdf

--- Личная безопасность/Перетаскивалка знаков/drag_task.js
+++ Личная безопасность/Перетаскивалка знаков/drag_task.js
@@ -12,20 +12,25 @@ function DragTask(options) {
     let task_scale = 1;
     let buttons_zone_height = settings.elements_height * 3;
     let buttons_templates = {};
-    Array.from(sourceSVG.getButtons().children).forEach(b=>buttons_templates[b.id] = b);
+    Array.from(sourceSVG.getButtons().children).forEach(b => buttons_templates[b.id] = b);
     let buttons = {};
     let dragObject = undefined;
     let dragUserObject = undefined;
+    let dragTaskBlock = undefined;
 
     let user_state = {
-      buttons: [],
-      scale: 1,
+        buttons: [],
+        scale: 1,
     };
 
     let test_scale = 1;
 
+    let elementStart = {x: 0, y: 0};
+    let mouseStart;
+
+    let pt;
     let DragTaskInit = {
-        init: function (elem){
+        init: function (elem) {
 
             let svg = utils.createElementSVG('svg', "mind_map_svg", null, {
                 viewbox: '0 0 ' + settings.width + ' ' + settings.height,
@@ -46,16 +51,23 @@ function DragTask(options) {
             task_block.appendChild(task_background);
             let task_img = sourceSVG.getTask().querySelector("g");
             task_block.appendChild(task_img);
-            utils.scaleElement(task_block,test_scale);
+            // task_block.onmousedown = function (e) {
+            //     dragTaskBlock = task_block;
+            //     start_drag_click = utils.cursorPoint(svg, e);
+            // };
+            // task_block.onmouseup = function () {
+            //     dragTaskBlock = false;
+            // };
+
             svg.appendChild(task_block);
 
             this.createTemplatesButtons(svg);
 
             this.createHandlers(svg);
-
+            pt = svg.createSVGPoint();
             elem.appendChild(svg)
         },
-        createHandlers: function(svg){
+        createHandlers: function (svg) {
 
             // addUserButton('btn_x5F_9', {x:20, y:100}, 'kekes');
 
@@ -63,11 +75,12 @@ function DragTask(options) {
                 buttons[item].onmousedown = function (e) {
                     buttons[item].setAttribute("cursor", "grabbing");
                     dragObject = buttons[item].cloneNode(true);
-
+                    elementStart = utils.getTransform(dragObject, "translate");
+                    mouseStart   = utils.cursorPoint(svg,e);
                     svg.querySelector("#task_block").appendChild(dragObject);
                     dragObject.onmouseup = function (e) {
-                        if (utils.getTranslation(dragObject).y < settings.height - buttons_zone_height - settings.elements_height / 2) {
-                            addUserButton(item, utils.getTranslation(dragObject), utils.getRandomString())
+                        if (utils.getTransform(dragObject, "translate").y < settings.height - buttons_zone_height - settings.elements_height / 2) {
+                            addUserButton(item, utils.getTransform(dragObject, "translate"), utils.getRandomString())
                         }
                         buttons[item].setAttribute("cursor", "grab");
                         dragObject.parentNode.removeChild(dragObject);
@@ -83,16 +96,15 @@ function DragTask(options) {
                 let t = buttons_templates[btn_class].cloneNode(true);
                 t.classList.add("user-button");
                 t.classList.remove("template-button");
-                utils.moveElement(t,coordinates.x,coordinates.y);
+                utils.moveElement(t, coordinates.x, coordinates.y);
                 let new_btn = {
                     button_id: id,
                     class: btn_class,
                     x: coordinates.x,
                     y: coordinates.y,
-                    html:t,
+                    html: t,
                 };
 
-                // console.log(new_btn);
                 new_btn.html.setAttribute("cursor", "grab");
 
                 let remove_btn = utils.createElementSVG('circle', '', 'remove-btn', {
@@ -102,7 +114,6 @@ function DragTask(options) {
                 });
 
                 remove_btn.onmousedown = function (e) {
-                    // console.log(new_btn);
                     if (confirm('Удалить элемент?')) {
                         new_btn.html.parentNode.removeChild(new_btn.html);
                         user_buttons = user_buttons.filter(item => item.button_id !== id)
@@ -119,8 +130,15 @@ function DragTask(options) {
                     if (e.target.classList.contains("remove-btn")) return false;
                     new_btn.html.setAttribute("cursor", "grabbing");
                     dragUserObject = new_btn.button_id;
-                    offset.x = e.offsetX - utils.getTranslation(user_buttons.filter(item => item.button_id === dragUserObject)[0].html).x;
-                    offset.y = e.offsetY - utils.getTranslation(user_buttons.filter(item => item.button_id === dragUserObject)[0].html).y;
+
+
+                    offset.x = e.offsetX - utils.getTransform(user_buttons.filter(item => item.button_id === dragUserObject)[0].html, "translate").x;
+                    offset.y = e.offsetY - utils.getTransform(user_buttons.filter(item => item.button_id === dragUserObject)[0].html, "translate").y;
+
+                    elementStart = utils.getTransform(new_btn.html, "translate");
+                    mouseStart   = utils.cursorPoint(svg,e);
+
+                    // console.log("elementStart", elementStart);
                 };
                 new_btn.html.onmouseup = function () {
                     offset.x = 0;
@@ -128,43 +146,109 @@ function DragTask(options) {
                     new_btn.html.setAttribute("cursor", "grab");
                     dragUserObject = undefined;
                 };
+
+
                 svg.querySelector("#task_block").appendChild(new_btn.html);
 
                 user_buttons.push(new_btn);
             }
 
-            svg.onmousemove = onMouseMove;
+
+            svg.querySelector("#task_block").addEventListener("mousewheel", ScrollFreeNodesHandler, false);
+
+            // svg.querySelector("#task-background").addEventListener("DOMMouseScroll", ScrollFreeNodesHandler, false);
+
+
+            function ScrollFreeNodesHandler(e) {
+                e = e || window.event;
+                var scroll_delta = e.deltaY || e.detail || e.wheelDelta;
+                if (e.deltaY && e) {
+                    test_scale = Math.round((test_scale + scroll_delta * 0.0005) * 100) / 100;
+                    if (test_scale < 0.5) test_scale = 0.5;
+                    if (test_scale > 2) test_scale = 2;
+                }
+                // console.log(test_scale)
+                utils.scaleElement(svg.querySelector("#task_block"), test_scale)
+                // drawMindMap(nodesObject, null, graph_scale);
+            }
+
+            document.onmousemove = onMouseMove;
+            document.onmouseup = function () {
+                dragUserObject = undefined;
+                return false;
+            };
 
             function onMouseMove(e) {
                 let mousePosition = utils.cursorPoint(svg, e);
+
+
                 let moveX = mousePosition.x;
                 let moveY = mousePosition.y;
 
+                let _x = moveX;
+                let _y = moveY;
+
+                if(dragObject || dragUserObject){
+
+                    let current = utils.cursorPoint(svg,e);
+                    pt.x = current.x - mouseStart.x;
+                    pt.y = current.y - mouseStart.y;
+
+                    let m;
+                    if(dragObject){
+                        // elementStart = utils.getTransform(dragObject, "translate");
+                        m = dragObject.getTransformToElement(svg).inverse();
+                    }
+                    else{
+                        // elementStart = utils.getTransform(user_buttons.filter(item => item.button_id === dragUserObject)[0].html, "translate");
+                        m = user_buttons.filter(item => item.button_id === dragUserObject)[0].html.getTransformToElement(svg).inverse();
+                    }
+                    m.e = m.f = 0;
+                    pt = pt.matrixTransform(m);
+
+                    _x = elementStart.x+pt.x;
+                    _y = elementStart.y+pt.y;
+                }
+
+
+
+                //
+                // console.log()
+                // console.log();
+
+                SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(elem) {
+                    return elem.getScreenCTM().inverse().multiply(this.getScreenCTM());
+                };
 
                 if (dragObject) {
-                    utils.moveElement(dragObject, moveX, moveY);
+                    utils.moveElement(dragObject, _x, _y);
                 } else if (dragUserObject) {
+
                     let current_elem = user_buttons.filter(item => item.button_id === dragUserObject)[0];
-                    if (moveX - offset.x > utils.getWidth(current_elem.html) / 2) {
-                        if (moveX - offset.x < settings.width - utils.getWidth(current_elem.html) / 2) {
-                            if (moveY - offset.y > settings.elements_height / 2) {
-                                if (moveY - offset.y < settings.height - buttons_zone_height - settings.elements_height / 2){
 
-                                    console.log("Без скейла", (moveX - offset.x),(moveY - offset.y) );
-                                    console.log("Co скейла", (moveX - offset.x)/test_scale,(moveY - offset.y)/test_scale );
+                    // if (moveX - offset.x > utils.getWidth(current_elem.html) / 2) {
+                    //     if (moveX - offset.x < settings.width - utils.getWidth(current_elem.html) / 2) {
+                    //         if (moveY - offset.y > settings.elements_height / 2) {
+                    //             if (moveY - offset.y < settings.height - buttons_zone_height - settings.elements_height / 2) {
 
-                                    utils.userMoveElement(current_elem, (moveX), (moveY));
-                                }
-                            }
-                        }
-                    }
+                    utils.userMoveElement(current_elem, _x, _y);
+                    //             }
+                    //         }
+                    //     }
+                    // }
                     return false;
+                } else if (dragTaskBlock) {
+                    // let mousePosition = utils.cursorPoint(svg, e);
+                    // let start_sdvig = {x: 0, y: 0};
+                    // let moveX = mousePosition.x - start_sdvig.x;
+                    // let moveY = mousePosition.y - start_sdvig.y;
+                    // dragTaskBlock.setAttribute("transform", "translate(" + moveX + "," + moveY + ")" + `scale(${test_scale})`);
                 } else {
                     return false;
                 }
             }
         },
-        createTemplatesButtons: function(svg){
+        createTemplatesButtons: function (svg) {
             let elements_zone = utils.createElementSVG('rect', "elements-background", null, {
                 width: settings.width,
                 y: settings.height - buttons_zone_height,
@@ -177,7 +261,7 @@ function DragTask(options) {
 
             // [].forEach.call(sourceSVG.getButtons().children, function (item, index) {
 
-            Object.keys(buttons_templates).forEach(function(item, index){
+            Object.keys(buttons_templates).forEach(function (item, index) {
                 buttons[item] = buttons_templates[item].cloneNode(true);
                 buttons[item].classList.add("template-button");
                 buttons[item].classList.add("drag-button");
@@ -194,6 +278,7 @@ function DragTask(options) {
         },
 
     };
+
     let utils = {
         createElementSVG: function (name, id, classList, attributes) {
             let NS = "http://www.w3.org/2000/svg";
@@ -207,7 +292,7 @@ function DragTask(options) {
             }
             return element;
         },
-        scaleElement: function(element, scale){
+        scaleElement: function (element, scale) {
             element.setAttribute("transform", `scale(${scale},${scale})`)
         },
         translateElement: function (element, x, y) {
@@ -215,44 +300,104 @@ function DragTask(options) {
             let el_width = element.querySelector("rect").getAttribute("width") / 2;
             element.setAttribute("transform", `translate(${x - el_width},${y - el_height})`)
         },
+        moveElement: function (element, x, y, user_elem) {
+            if (x < this.getWidth(element) / 2) x = this.getWidth(element) / 2;
+            if (x > settings.width - this.getWidth(element) / 2) x = settings.width - this.getWidth(element) / 2;
+            if (y < settings.elements_height / 2) y = settings.elements_height / 2;
+            if (y > settings.height - (user_elem ? buttons_zone_height : 0) - settings.elements_height / 2) y = settings.height - (user_elem ? buttons_zone_height : 0) - settings.elements_height / 2;
 
-        moveElement: function (element, x, y) {
-            element.setAttribute("transform", `translate(${x/test_scale},${y/test_scale})`)
+            element.setAttribute("transform", `translate(${x},${y})`);
+            this.createAnswer();
         },
-
         userMoveElement: function (object, x, y) {
-            this.moveElement(object.html, x, y);
-            let c = this.getTranslation(object.html);
+            this.moveElement(object.html, x, y, true);
+            let c = this.getTransform(object.html, "translation");
             object.x = c.x;
             object.y = c.y;
         },
-
         getWidth: function (element) {
             return element.querySelector("rect").getAttribute("width");
         },
-
         cursorPoint: function (SVGObject, event) {
-            let pt = SVGObject.createSVGPoint();
+            pt = SVGObject.createSVGPoint();
             pt.x = event.clientX;
             pt.y = event.clientY;
             return pt.matrixTransform(SVGObject.getScreenCTM().inverse());
         },
-
-        getTranslation: function (element) {
-            let coords = {x: 0, y: 0};
+        // DragTarget: function (SVGObject, event) {
+        //     var pnt = SVGObject.ownerSVGElement.createSVGPoint();
+        //     // console.log(DragTarget)
+        //     pnt.x = event.clientX;
+        //     pnt.y = event.clientY;
+        //     //---elements in different(svg) viewports, and/or transformed ---
+        //     var sCTM = SVGObject.getScreenCTM();
+        //     // console.log(sCTM)
+        //     var Pnt = pnt.matrixTransform(sCTM.inverse());
+        //     return Pnt;
+        //     // return pt.matrixTransform(SVGObject.getScreenCTM().inverse());
+        // },
+        getTransform: function (element, type) {
+            let output = {x: 0, y: 0};
+            let full_output = {};
             if (element.hasAttribute("transform")) {
-                if (element.getAttribute("transform").includes('translate')) {
-                    let c = element.getAttribute("transform").replace('translate(', '').replace(')', '').split(',').map(x => Number(x));
-                    coords.x = c[0];
-                    coords.y = c[1];
+                full_output = utils.parseTransform(element.getAttribute("transform"));
+            }
+            if (full_output[type]) {
+                output = full_output[type]
+            }
+
+            return output;
+        },
+        parseTransform: function (input_str) {
+            var prop = ['translate', 'matrix', 'rotate', 'skewX', 'skewY', 'scale'];
+            var val = input_str.match(/(translate|matrix|rotate|skewX|skewY|scale)\(.*?\)/g);
+            var obj = {};
+            if (val) {
+                for (var i = 0, length = val.length; i < length; i++) {
+                    var item = val[i];
+                    var index = item.indexOf('(');
+                    var v = item.substring(index + 1, item.length - 1).split(/\,|\s/);
+                    var n = item.substring(0, index);
+                    obj[n] = {};
+                    switch (n) {
+                        case 'translate':
+                        case 'scale':
+                            obj[n].x = +v[0] || 0;
+                            obj[n].y = +v[1] || 0;
+                            break;
+                        case 'rotate':
+                            obj[n].a = +v[0] || 0;
+                            obj[n].x = +v[1] || 0;
+                            obj[n].y = +v[2] || 0;
+                            break;
+                        case 'skewX':
+                        case 'skewY':
+                            obj[n].a = +v[0];
+                            break;
+                        case 'matrix':
+                            obj[n].a = +v[0] || 0;
+                            obj[n].b = +v[1] || 0;
+                            obj[n].c = +v[2] || 0;
+                            obj[n].d = +v[3] || 0;
+                            obj[n].e = +v[4] || 0;
+                            obj[n].f = +v[5] || 0;
+                            break;
+                    }
                 }
             }
-            return coords;
+            return obj;
         },
         getRandomString: function (length) {
             return Math.random().toString(36).substr(2, (length || 8));
         },
+        createAnswer() {
+            user_state.buttons = user_buttons.map(function (item) {
+                return {x: item.x, y: item.y, class: item.class, button_id: item.button_id};
+            });
+            user_state.scale = test_scale || 1;
+        },
     };
+
 
     DragTaskInit.init(task_container)
 
